ea581576739d3a04a984f4a177b9c10baa8b2f0e
Romit  Dey <romit77dey@gmail.com>
2025-08-16 17:30:19 +0530

    fix ui bug

--- app/(platform)/(dashboard)/layout.tsx
+++ app/(platform)/(dashboard)/layout.tsx
@@ -2,9 +2,9 @@ import { Navbar } from "./_components/navbar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full">
+    <div className="min-h-screen">
       <Navbar />
-      {children}
+      <main className="pt-14 min-h-screen">{children}</main>
     </div>
   );
 };
